abfbb41a56003499e5d0957908d9d9f007a18f85
vishal <Vishalgehlot334@gmail.com>
2023-02-22 16:50:06 +0530

    static authentication is added

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
-
-
-const submitform =()=>{
+const submitform =(event)=>{
+   
+    event.preventDefault();
     let email=document.getElementById('email').value
     let password=document.getElementById('pass').value
     console.log(email,password)
@@ -9,6 +8,7 @@ if(email=="fullstack@gmail.com" && password=="123456")
 {
    alert('success')
     document.getElementById('change').style.background="red"
+    location.href="http://127.0.0.1:5500/dashboard.html"
 }
 else{
     alert('wrong credentials')
